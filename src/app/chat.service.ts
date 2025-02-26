3807f98b5b1e642f405f24a201d2b2cadfc54e1d
gutsnpak <gutsnpak@gmail.com>
2025-02-26 15:29:33 +0300

    Update chat.service.ts

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -8,25 +8,25 @@ export class ChatService {
   private channel: BroadcastChannel;
   private messagesSubject: Subject<{ user: string, text: string, time: string }> = new Subject<{ user: string, text: string, time: string }>();
   private messagesArray: { user: string, text: string, time: string }[] = [];
+
   public messages: Observable<{ user: string, text: string, time: string }> = this.messagesSubject.asObservable();
 
   constructor() {
     this.channel = new BroadcastChannel('swoyochat_channel');
+
     this.channel.onmessage = (event) => {
       const message = event.data;
       this.messagesArray.push(message);
       this.messagesSubject.next(message);
-      this.updateLocalStorage();
     };
+
+    setInterval(() => {
+      localStorage.setItem('chatMessages', JSON.stringify(this.messagesArray));
+    }, 1000);
   }
 
   sendMessage(message: { user: string, text: string, time: string }) {
     this.channel.postMessage(message);
     this.messagesArray.push(message);
-    this.updateLocalStorage();
-  }
-
-  private updateLocalStorage() {
-    localStorage.setItem('chatMessages', JSON.stringify(this.messagesArray));
   }
 }
