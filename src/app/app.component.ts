ebd48fc378c4c3f09d1b67cc0a08093f31936f94
gutsnpak <gutsnpak@gmail.com>
2025-02-27 11:06:32 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatService } from './chat.service';
-import { FormsModule } from '@angular/forms';
-import { NgForOf, NgIf } from '@angular/common';
-import { AuthService } from './auth.service';
+import {FormsModule} from '@angular/forms';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,8 @@ import { AuthService } from './auth.service';
   imports: [
     FormsModule,
     NgForOf,
-    NgIf
+    NgIf,
+    AsyncPipe
   ],
   styleUrls: ['./app.component.css']
 })
@@ -19,36 +20,26 @@ export class AppComponent implements OnInit {
   newMessage: string = '';
   username: string = '';
 
-  constructor(private chatService: ChatService, protected authService: AuthService, private cdr: ChangeDetectorRef) {
-  }
 
-  ngOnInit() {
-    this.loadMessages();
-    this.authService.clearUsername();
-    const storedUsername = this.authService.getUsername();
-    if (storedUsername) {
-      this.username = storedUsername;
+constructor(private chatService: ChatService, protected authService: AuthService) {}
+    ngOnInit() {
+
+      this.loadMessages();
+      this.authService.clearUsername();
+      const storedUsername = this.authService.getUsername();
+      if (storedUsername) {
+        this.username = storedUsername;
+      }
+
+      this.chatService.messages$.subscribe((messages: { user: string; text: string; time: string }[]) => {
+        this.messages = messages.slice();
+      });
     }
-    this.chatService.messages.subscribe((message: { user: string, text: string, time: string }) => {
-      this.messages.push(message);
-      this.saveMessages();
-      this.cdr.detectChanges();
-    });
 
-    setTimeout(() => {
-      this.cdr.detectChanges();
-    }, 1000);
-  }
-
-  sendMessage() {
+   sendMessage() {
     if (this.newMessage.trim() && this.username) {
-      const message = {
-        user: this.username,
-        text: this.newMessage,
-        time: new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
-      };
+      const message = { user: this.username, text: this.newMessage, time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) };
       this.chatService.sendMessage(message);
-      this.messages.push(message);
       this.saveMessages();
       this.newMessage = '';
     }
@@ -64,8 +55,10 @@ export class AppComponent implements OnInit {
       this.messages = JSON.parse(savedMessages);
     }
   }
-
-  login() {
+    get messages$() {
+    return this.chatService.messages$;
+  }
+    login() {
     if (this.username && this.username.trim()) {
       this.authService.setUsername(this.username);
     }
